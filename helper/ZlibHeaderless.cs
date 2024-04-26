5f5a864c91d592717d932b3c4fa2489078ac4c09
Alexander Brunner <git@alexanderbrunner.net>
2024-04-26 10:19:48 +0200

    Added headerless deflate for c#

---
+++ helper/ZlibHeaderless.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AoEKaitai
+{
+    public class ZlibHeaderless : Kaitai.CustomDecoder
+    {
+        public byte[] Decode(byte[] src)
+        {
+            using (var ms = new MemoryStream(src))
+            using (var decompressor = new System.IO.Compression.DeflateStream(ms, System.IO.Compression.CompressionMode.Decompress))
+            using (var decompressed = new MemoryStream())
+            {
+                decompressor.CopyTo(decompressed);
+                return decompressed.ToArray();
+            }
+                
+        }
+    }
+}
